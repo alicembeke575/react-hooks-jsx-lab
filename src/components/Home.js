6e08a54dd42050f7f0918ee3b8d9af1288685e39
Alice Mbeke <alicembeke575@gmail.com>
2022-07-19 19:04:02 +0300

    assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div id="home">
       <h1 style={{ color: "firebrick"}}>
-        {name} is a web developer from {city}
+        {name } is a Web Developer from {city}
       </h1>
     </div>
   )
